9e44b01864c2352d1a97a4cff698830fd37462dd
kitom <mormonbook2013@yahoo.ca>
2021-08-12 12:39:02 -0400

    initial Commit: Basic structure and simple shape classes. Test don't pass.

---
+++ src/zinga/Hello.java
@@ -0,0 +1,12 @@
+package zinga;
+
+public class Hello {
+    public void sayHello(String name){
+        System.out.println("Hello, my name is " + name + "!");
+    }
+    public static void main(String[] args){
+        Hello hello = new Hello();
+        hello.sayHello( "Francisco");
+
+    }
+}
